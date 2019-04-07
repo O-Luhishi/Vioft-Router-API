3f42ab34e273ac0b196395c837c521d53e86f4f9
Oluhishi <oeluhishi@gmail.com>
2019-04-07 18:06:49 +0100

    Structured project for C

---
+++ List_All_Clients/C-Implementation/demo.h
@@ -0,0 +1,16 @@
+//
+// Created by Osama luhishi on 04/04/2019.
+//
+
+#ifndef C_IMPLEMENTATION_DEMO_H
+#define C_IMPLEMENTATION_DEMO_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <json-c/json.h>
+
+int demo_hello(json_object* input, json_object* output);
+
+#endif //C_IMPLEMENTATION_DEMO_H
---
+++ List_All_Clients/C-Implementation/main.c
@@ -0,0 +1,10 @@
+#include "demo.h"
+
+int main() {
+    printf("Hello!\n");
+    return 0;
+}
+
+int demo_hello(json_object* input, json_object* output){
+    return 0;
+}
